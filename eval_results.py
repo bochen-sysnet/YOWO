c6070580f314cce6d48cb70ade6a2ebf062dd066
Bo Chen <bochen1993cs@gmail.com>
2020-09-21 22:07:48 -0500

    tmp version

--- eval_results.py
+++ eval_results.py
@@ -175,7 +175,6 @@ def video_ap_one_class(gt, pred_videos, potential_class, iou_thresh = 0.2, bTemp
                 if g[0] == video_index:
                     gt_this.append(g[1])
                     gt_this_index.append(j)
-            print(len(gt_this))
             if len(gt_this) > 0:
                 if bTemporal:
                     iou = np.array([iou3dt(np.array(g), boxes[:, :5]) for g in gt_this])
@@ -188,7 +187,7 @@ def video_ap_one_class(gt, pred_videos, potential_class, iou_thresh = 0.2, bTemp
                         iou = np.array([iou3d(g[int(boxes[0,0]-1):int(boxes[-1,0]),:], boxes[:,:5]) for g in gt_this]) 
                     else:
                         iou = np.array([iou3d(g, boxes[:,:5]) for g in gt_this]) 
-                print(iou)
+                print(iou, iou.size, iou[argmax], iou_thresh)
                 if iou.size > 0: # on ucf101 if invalid annotation ....
                     argmax = np.argmax(iou)
                     # check if this det tube matches any gt tube
