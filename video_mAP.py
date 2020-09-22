f30712e94d248832998ccf5fd6076a766d27ffca
Bo Chen <bochen1993cs@gmail.com>
2020-09-21 20:28:01 -0500

    tmp version

--- video_mAP.py
+++ video_mAP.py
@@ -271,6 +271,7 @@ def video_mAP_jhmdb():
     detected_boxes = {}
     gt_videos = {}
     bbx_det_start = time.perf_counter()
+    nl = 0
     for line in lines:
         print(line)
 
@@ -338,6 +339,8 @@ def video_mAP_jhmdb():
         v_annotation['gt_classes'] = t_label
         v_annotation['tubes'] = np.expand_dims(np.array(all_gt_boxes), axis=0)
         gt_videos[video_name] = v_annotation
+        nl += 1
+        if nl == 10:break
 
     bbx_det_end = time.perf_counter()
     bbx_pred_t = bbx_det_end - bbx_det_start
