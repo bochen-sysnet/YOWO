b5505c6cc84a3e05b45332d9cb8c73b19269c2ac
Bo Chen <bochen1993cs@gmail.com>
2020-10-07 17:27:27 -0500

    Update video_mAP.py

--- video_mAP.py
+++ video_mAP.py
@@ -203,7 +203,7 @@ def extract_n_filter_one_batch(batch, prev_frame):
         edge_feat_list.append(get_edge_feature(prev_frame))
         area_feat_list.append(get_area_feature(prev_frame))
     for i in range(batch.size(0)):
-        rgb_frame = batch[i,:,:,:].squeeze(0).numpy()
+        rgb_frame = batch[i,:,:,:].squeeze(0).permute(1, 2, 0).numpy()
         bgr_frame = cv2.cvtColor(rgb_frame, cv2.COLOR_RGB2BGR)
         # pixel diff
         pixel_feat = get_pixel_feature(bgr_frame)
