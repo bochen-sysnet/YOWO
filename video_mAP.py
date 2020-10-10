df2f8751f5a06a890d24dd0bbfc69522be10d7b3
Bo Chen <bochen1993cs@gmail.com>
2020-10-10 16:08:29 -0500

    Update video_mAP.py

--- video_mAP.py
+++ video_mAP.py
@@ -358,7 +358,7 @@ def video_mAP_ucf():
         if iou_th == iou_list[0]:
             with open(tube_score_file,'wb') as f:
                 for line in all_tube_scores:
-                    np.savetxt(f, line, fmt='%.3f')
+                    np.savetxt(f, line.reshape(1, line.shape[0]), fmt='%.3f')
 
 
 def video_mAP_jhmdb():
@@ -461,7 +461,6 @@ def video_mAP_jhmdb():
         gt_videos[video_name] = v_annotation
         
         all_feat[lidx,:] = [np.mean(pixel_diff_list), np.mean(edge_diff_list), np.mean(area_diff_list)]
-        break
 
     bbx_det_end = time.perf_counter()
     bbx_pred_t = (bbx_det_end - bbx_det_start)
