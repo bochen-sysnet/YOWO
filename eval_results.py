98cb7978fc6cde2c7d74365cbbb7baccf9f80218
Bo Chen <bochen1993cs@gmail.com>
2020-10-21 10:20:40 -0500

    Update eval_results.py

--- eval_results.py
+++ eval_results.py
@@ -336,6 +336,7 @@ def evaluate_videoAP(gt_videos, all_boxes, CLASSES, bbx_pred_t, iou_thresh = 0.2
         return res, v_cnt
 
     print_str = ""
+    log_str = ""
     gt_videos_format = gt_to_videts(gt_videos)
     pred_videos_format, v_cnt = imagebox_to_videts(all_boxes, CLASSES, skip_cnt)
     # predict potential classes of each video based on first few frames
@@ -379,22 +380,18 @@ def evaluate_videoAP(gt_videos, all_boxes, CLASSES, bbx_pred_t, iou_thresh = 0.2
     act_loc_t_new = bbx_pred_t/v_cnt + cls_pred_t + np.sum(actual_t_all)
     print_str += "{0:.3f}\t{1:.3f}\t".format(np.mean(ap_all), np.mean(ap_new_all))
     print_str += "{0:.3f}\t{1:.3f}\n".format(act_loc_t_old, act_loc_t_new)
-    #ealr_old = np.log(np.mean(ap_all))/act_loc_t_old
-    #ealr_new = np.log(np.mean(ap_new_all))/act_loc_t_new
-    #print_str += "{0:.4f}\t{1:.4f}\t{2:.4f}\t".format(ealr_old, ealr_new, ealr_new/ealr_old)
-    #saved_link_time_ratio = (np.sum(raw_t_all) - cls_pred_t - np.sum(actual_t_all))/(np.sum(raw_t_all))
-    #saved_al_time_ratio = (np.sum(raw_t_all) - cls_pred_t - np.sum(actual_t_all))/act_loc_t_old
-    #print_str += "{0:.4f}\t{1:.4f}\t".format(saved_link_time_ratio, saved_al_time_ratio)
-    #print_str += str(np.sum(missed_actions_all)) + '/' + str(np.sum(gt_actions_all)) + '\n'
-    #saved_link_t_ratio_wrt_class = (np.cumsum(raw_t_all) - cls_pred_t - np.cumsum(actual_t_all))/(np.cumsum(raw_t_all))
-    #saved_al_time_ratio_wrt_class = (np.cumsum(raw_t_all) - cls_pred_t - np.cumsum(actual_t_all))/(np.cumsum(raw_t_all) + bbx_pred_t/v_cnt)
-    #print_str += "Saved time:\n"
-    #print_str += str(saved_link_t_ratio_wrt_class) + '\n'
-    #print_str += str(saved_al_time_ratio_wrt_class) + '\n'
-    #old_link_t_wrt_class = np.cumsum(raw_t_all)
-    #new_link_t_wrt_class = cls_pred_t + np.cumsum(actual_t_all)
-    #print_str += "BBX Pred Time:{0:.4f}\n".format(bbx_pred_t/v_cnt)
-    #print_str += str(old_link_t_wrt_class) + '\n'
-    #print_str += str(new_link_t_wrt_class) + '\n'
+    saved_link_time_ratio = (np.sum(raw_t_all) - cls_pred_t - np.sum(actual_t_all))/(np.sum(raw_t_all))
+    saved_al_time_ratio = (np.sum(raw_t_all) - cls_pred_t - np.sum(actual_t_all))/act_loc_t_old
+    log_str += "{0:.4f}\t{1:.4f}\t".format(saved_link_time_ratio, saved_al_time_ratio)
+    log_str += str(np.sum(missed_actions_all)) + '/' + str(np.sum(gt_actions_all)) + '\n'
+    saved_link_t_ratio_wrt_class = (np.cumsum(raw_t_all) - cls_pred_t - np.cumsum(actual_t_all))/(np.cumsum(raw_t_all))
+    saved_al_time_ratio_wrt_class = (np.cumsum(raw_t_all) - cls_pred_t - np.cumsum(actual_t_all))/(np.cumsum(raw_t_all) + bbx_pred_t/v_cnt)
+    log_str += str(saved_link_t_ratio_wrt_class) + '\n'
+    log_str += str(saved_al_time_ratio_wrt_class) + '\n'
+    old_link_t_wrt_class = np.cumsum(raw_t_all)
+    new_link_t_wrt_class = cls_pred_t + np.cumsum(actual_t_all)
+    log_str += "BBX Pred Time:{0:.4f}\n".format(bbx_pred_t/v_cnt)
+    log_str += str(old_link_t_wrt_class) + '\n'
+    log_str += str(new_link_t_wrt_class) + '\n'
     
-    return print_str, tube_scores_all
+    return print_str, log_str, tube_scores_all
