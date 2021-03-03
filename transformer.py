85ae758422f3ac3a1d473234c2500f521c380a29
Bo Chen <bochen1993cs@gmail.com>
2021-03-02 23:02:29 -0600

    test lru

--- transformer.py
+++ transformer.py
@@ -274,7 +274,7 @@ class Transformer:
 		# !!!!!!!!!! some problems with lru
 		if img_index in self.lru: 
 			im1 = self.lru[img_index]
-			im2 = path_to_disturbed_image(image, label, 1, 1)
+			im2 = image #path_to_disturbed_image(image, label, 1, 1)
 			diff = ImageChops.difference(im1, im2).getbbox()
 			print(img_index,diff)
 			return im2
@@ -320,7 +320,7 @@ class Transformer:
 		# use f(x)=A*e^(-sigma*(1-x)) to calculate a quality from the score
 		# downsample the image based on the quality
 
-		image = path_to_disturbed_image(image, label, 1, 1)
+		# image = path_to_disturbed_image(image, label, 1, 1)
 		self.lru[img_index] = image
 		return self.lru[img_index]
 
