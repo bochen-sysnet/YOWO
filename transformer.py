4986b40469b8eaa5865b8a2bb0a16b221756e280
Bo Chen <bochen1993cs@gmail.com>
2021-03-02 22:53:21 -0600

    test lru

--- transformer.py
+++ transformer.py
@@ -323,7 +323,7 @@ class Transformer:
 		# downsample the image based on the quality
 
 		image = path_to_disturbed_image(image, label, 1, 1)
-		self.lru[img_index] = image
+		self.lru[img_index] = image.copy()
 		return Image.fromarray(image)
 
 if __name__ == "__main__":
