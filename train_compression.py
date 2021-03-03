a4ef57fe68b53ee943f5762e575f794f0d6a3dc3
Bo Chen <bochen1993cs@gmail.com>
2021-03-02 20:54:55 -0600

    test 20 1,0.5

--- train_compression.py
+++ train_compression.py
@@ -9,7 +9,8 @@ from transformer import Transformer
 if __name__ == "__main__":
     TF = Transformer('compression')
     # the first 6 parameters are the weights of 7 features
-    # the 7th parameter is for the value of the function at 0
+    # the 7th parameter is for the value of the function when score=1
+    # the 8th parameter is the sigma of the function
     C_param = np.zeros(8)
     sim_result = simulate('ucf101-24', data_range=(0,20), TF=TF, C_param=C_param)
     print(sim_result)
--- transformer.py
+++ transformer.py
@@ -269,19 +269,20 @@ class Transformer:
 		self.lru = LRU(16) # size of clip
 
 	def transform(self, image=None, label=None, C_param=None, img_index=None):
+		# Rule 1: 
 		# analyze features in image, 
 		# divide [320,240] image to 8*6 tiles
 		# count distribution of features in 48 tiles (normalized sum to 1)
 		# get weighted sum of distribution of features, which is the score of each tile
 		# 0 <= score <= 1
-		# use f(x)=e^(-sigma*x) to 
+		# use f(x)=A*e^(-sigma*(1-x)) to calculate a quality from the score
 		# derive the quality in each tile based on the compression param
 
 		# downsample the image based on the quality
 		# if img_index in self.lru:
 		# 	image = self.lru[img_index]
 		# else:
-		image = path_to_disturbed_image(image, label, .2, .2)
+		image = path_to_disturbed_image(image, label, 1, 0.5)
 		# self.lru[img_index] = image
 		return image
 
