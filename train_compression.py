17e47bc9e35c4f68833125c0398a95f46df48511
Bo Chen <bochen1993cs@gmail.com>
2021-03-04 08:25:25 -0600

    test rsnet

--- train_compression.py
+++ train_compression.py
@@ -29,12 +29,20 @@ class C_Generator:
 		pass
 
 	def get(self):
-	    # the first 6 parameters are the weights of 6 features (-1,1)
-	    # the 7th parameter is max ratio
-	    # the 8th parameter is multiplier
-	    # the 9th parameter is order
+	    # the first 6 parameters are the weights of 6 features (0,1)
+	    # the 7th parameter is max ratio (0,1)
+	    # the 8th parameter is multiplier (0,1)
+	    # the 9th parameter is order [-3,3]
 	    # dirichlet?
-		return np.array([1,1,2,2,3,3,1,1,-3],dtype=np.float64)
+	    C_param = uniform_init_gen()
+		return C_param
+
+	def uniform_init_gen(self):
+		output = np.zeros(9,dtype=np.float64)
+		output[:8] = np.random.randint(0,10,8)/10
+		output[8] = np.random.randint(-3,3)
+		return output
+
 
 def train(net):
 	np.random.seed(123)
@@ -42,10 +50,11 @@ def train(net):
 	optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 	# setup
-	num_batch = 1
 	range_size = 10 # number of videos we test
+	video_num = 137557 # 9126 for J-HMDB
 	batch_size = 4
-	print_step = 10
+	num_batch = 2 # video_num//(batch_size*range_size)
+	print_step = 1
 	eval_step = 1
 
 	for epoch in range(1):
@@ -55,10 +64,12 @@ def train(net):
 
 		for bi in range(num_batch):
 			inputs,labels = [],[]
-			for di in range(batch_size):
-				data_range = (bi*range_size,(bi+1)*range_size)
+			for k in range(batch_size):
+				di = bi*batch_size + k # data index
+				data_range = (di*range_size,di*range_size+range_size)
 				C_param = cgen.get()
 				sim_result = simulate('ucf101-24', data_range=data_range, TF=TF, C_param=C_param)
+				print(data_range,C_param,sim_result)
 				inputs.append(C_param)
 				labels.append(sim_result[0][4]) # accuracy of IoU=0.5
 			inputs = torch.FloatTensor(inputs).cuda()
@@ -75,9 +86,10 @@ def train(net):
 
 			# print statistics
 			running_loss += loss.item()
-			if i % print_step == print_step-1:    # print every 200 mini-batches
+			if i % print_step == 0 and print_step>0:    
 				print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / print_step))
 				running_loss = 0.0
+		# evaluation
 		if epoch%eval_step==0 and epoch>0:
 			validate(net)
 	PATH = 'backup/rsnet.pth'
@@ -89,10 +101,12 @@ def validate(net):
 	np.random.seed(321)
 	with torch.no_grad():
 		# setup
-		num_batch = 1
 		range_size = 10 # number of videos we test
-		batch_size = 4	
-		print_step = 10
+		video_num = 137557 # 9126 for J-HMDB
+		batch_size = 4
+		num_batch = 1 # video_num//batch_size
+		print_step = 1
+		eval_step = 1
 
 		for epoch in range(1):
 			running_loss = 0.0
@@ -119,7 +133,7 @@ def validate(net):
 
 				# print statistics
 				running_loss += loss.item()
-				if i % print_step == print_step-1:    # print every 200 mini-batches
+				if i % print_step == 0:    # print every 200 mini-batches
 					print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / print_step))
 					running_loss = 0.0
 
@@ -127,17 +141,11 @@ def validate(net):
 if __name__ == "__main__":
 	# prepare network
 	net = RSNet([8,255,255,1])
-	print(net)
 	net = net.cuda()
-	random_data = torch.rand((8)).cuda()
-	print(random_data)
-	result = net(random_data)
-	print(result)
-	exit(0)
 	train(net)
 
-	cgen = C_Generator()
-	TF = Transformer('compression')
-	C_param = cgen.get()
-	sim_result = simulate('ucf101-24', data_range=(0,1), TF=TF, C_param=C_param)
-	print(sim_result)
+	# cgen = C_Generator()
+	# TF = Transformer('compression')
+	# C_param = cgen.get()
+	# sim_result = simulate('ucf101-24', data_range=(0,1), TF=TF, C_param=C_param)
+	# print(sim_result)
