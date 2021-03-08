d121bf8c836cb7b47c30b8c4e725cd077b932cad
Bo Chen <bochen1993cs@gmail.com>
2021-03-07 19:30:59 -0600

    change upperlower

--- transformer.py
+++ transformer.py
@@ -304,6 +304,7 @@ def tile_disturber(image, C_param):
 	# lower and upper
 	lower,upper = C_param[num_features:num_features+2] + 0.5
 	lower,upper = min(lower,upper),max(lower,upper)
+	lower = max(lower,0);upper = min(upper,1)
 	# order to adjust the concentration of the  scores
 	k = int(((C_param[num_features+2]+0.5)*5))
 	k = min(k,4); k = max(k,0)
