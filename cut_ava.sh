b789ea5f0774cd13a8a9b02d523b1a0a84b176e2
Bo Chen <bochen1993cs@gmail.com>
2021-06-24 14:42:28 -0500

    modify ucf24 path

---
+++ cut_ava.sh
@@ -0,0 +1,16 @@
+IN_DATA_DIR="../dataset/ava/videos"
+OUT_DATA_DIR="../dataset/ava/videos_15min"
+
+if [[ ! -d "${OUT_DATA_DIR}" ]]; then
+  echo "${OUT_DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${OUT_DATA_DIR}
+fi
+
+for video in $(ls -A1 -U ${IN_DATA_DIR}/*)
+do
+  out_name="${OUT_DATA_DIR}/${video##*/}"
+  if [ ! -f "${out_name}" ]; then
+  	echo ffmpeg -ss 900 -t 901 -i "${video}" "${out_name}"
+    ffmpeg -ss 900 -t 901 -i "${video}" "${out_name}"
+  fi
+done
