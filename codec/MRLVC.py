7a3223fa3df0202ecaffbd0572a07a0921e52fe7
Bo Chen <bochen1993cs@gmail.com>
2021-07-08 17:05:26 -0500

    test

--- codec/MRLVC.py
+++ codec/MRLVC.py
@@ -91,11 +91,11 @@ class MRLVC(nn.Module):
         Y1_warp = F.grid_sample(Y0_com, loc + mv_hat.permute(0,2,3,1))
         MC_input = torch.cat((mv_hat, Y0_com, Y1_warp), axis=1)
         Y1_MC = self.MC_network(MC_input.cuda(1))
-        # # compress residual
-        # res = Y1_raw.cuda(1) - Y1_MC
-        # res_hat,res_latent_hat,res_hidden,likelihoods = self.res_codec(res, res_hidden.cuda(1), RPM_flag)
-        # # reconstruction
-        # Y1_com = torch.clip(res_hat + Y1_MC, min=0, max=1)
+        # compress residual
+        res = Y1_raw.cuda(1) - Y1_MC
+        res_hat,res_latent_hat,res_hidden,likelihoods = self.res_codec(res, res_hidden.cuda(1), RPM_flag)
+        # reconstruction
+        Y1_com = torch.clip(res_hat + Y1_MC, min=0, max=1)
         # if RPM_flag:
         #     # latent presentations
         #     prior_mv_latent, prior_res_latent = torch.split(prior_latent.cuda(1),128,dim=1)
