ca00354afb0be131439b790bce83b87511c05715
Bo Chen <bochen1993cs@gmail.com>
2021-07-07 21:43:02 -0500

    test

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -69,6 +69,7 @@ def train_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, train_loader, loss
             Y0_com = None
             for j in range(data.size(2)):
                 Y1_raw = data[i,:,j,:,:].unsqueeze(0)
+                print(Y1_raw.shape)
                 if indices[j]%10 == 1:
                     # no need for Y0_com, latent, hidden when compressing
                     # the I frame 
