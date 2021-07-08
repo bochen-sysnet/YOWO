57a3319e0c2f4ef5c299812460548cce56234154
Bo Chen <bochen1993cs@gmail.com>
2021-07-07 21:15:31 -0500

    test

--- codec/MRLVC.py
+++ codec/MRLVC.py
@@ -62,7 +62,7 @@ class MRLVC(nn.Module):
                 else:
                     metrics = MSSSIM(Y1_raw, Y1_com)
                     loss = 32*(1-metrics) + bpp_est
-                print('ok')
+                print('ok',Y1_com.shape,loss)
                 return Y1_com, loss, bpp_est, bpp_act, metrics
             else:
                 # no compression
