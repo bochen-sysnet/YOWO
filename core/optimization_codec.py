912ff78ff95c6fd7ebc60fa636613ba74886de5c
Bo Chen <bochen1993cs@gmail.com>
2021-07-08 19:46:57 -0500

    test

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -98,11 +98,10 @@ def train_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, train_loader, loss
             com_clip = torch.cat(com_clip,dim=0).permute(1, 0, 2, 3).unsqueeze(0)
             com_data.append(com_clip)
             # extract the compression metrics
-        com_data = torch.cat(com_data,dim=0)
-        com_data = data[:,:,9:25,:,:]
+        data = torch.cat(com_data,dim=0)
         print(com_data.shape)
         # end encoding
-        output = model(com_data)
+        output = model(data)
         loss = loss_module(output, target, epoch, batch_idx, l_loader)
 
         loss.backward()
