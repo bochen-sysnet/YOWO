284d5abec1a246cecd0ad0bb948ad5d8ed015ccd
Bo Chen <bochen1993cs@gmail.com>
2021-08-28 08:20:42 -0500

    Update MRLVC.py

--- codec/MRLVC.py
+++ codec/MRLVC.py
@@ -52,9 +52,9 @@ class MRLVC(nn.Module):
                 RPM_flag, I_flag, use_psnr=True): 
         # this just for testing without compression
         if I_flag:
-            return Y1_raw,torch.zeros(1),torch.zeros(1),torch.zeros(1),torch.zeros(1)
+            return Y1_raw,torch.zeros(1).cuda(0),torch.zeros(1).cuda(0),torch.zeros(1),torch.zeros(1)
         else:
-            return Y1_raw.cuda(0), rae_hidden, rpm_hidden, prior_latent, torch.zeros(1),torch.zeros(1),torch.zeros(1),torch.zeros(1)
+            return Y1_raw.cuda(0), rae_hidden, rpm_hidden, prior_latent, torch.zeros(1).cuda(0),torch.zeros(1).cuda(0),torch.zeros(1),torch.zeros(1)
         # Y0_com: compressed previous frame
         # Y1_raw: uncompressed current frame
         # RPM flag: whether the first P frame (0: yes, it is the first P frame)
