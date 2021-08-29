6ae26fc232239db7d775a33075a3fff7816053f4
Bo Chen <bochen1993cs@gmail.com>
2021-08-29 10:46:37 -0500

    eval on train

--- main_codec.py
+++ main_codec.py
@@ -138,7 +138,7 @@ elif dataset in ['ucf24', 'jhmdb21']:
 # ---------------------------------------------------------------
 if cfg.TRAIN.EVALUATE:
     logging('evaluating ...')
-    test(cfg, 0, model, model_codec, test_dataset, loss_module)
+    test(cfg, 0, model, model_codec, train_dataset, loss_module)
 else:
     for epoch in range(cfg.TRAIN.BEGIN_EPOCH, cfg.TRAIN.END_EPOCH + 1):
         # Adjust learning rate
