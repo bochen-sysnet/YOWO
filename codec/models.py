16e1553775f4f9c5ea3a75126b3e5b8320db517a
Bo Chen <bochen1993cs@gmail.com>
2021-09-03 14:09:06 -0500

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -277,6 +277,7 @@ class StandardVideoCodecs(nn.Module):
                 clip.append(img)
             # When everything done, release the video capture object
             cap.release()
+            print(len(clip), len(raw_clip))
             assert len(clip) == len(raw_clip), 'Clip size mismatch'
             # create cache
             cache['clip'] = clip
