ea353ddb5caaaf254f6284b538b56b78b25923ce
Bo Chen <bochen1993cs@gmail.com>
2021-09-17 19:33:52 -0500

    Update optimization_codec.py

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -86,8 +86,8 @@ def train_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, train_dataset, los
                 img_loss = torch.FloatTensor([0]).cuda(0)
             else:
                 reg_loss = torch.FloatTensor([0]).cuda(0)
-                reg_loss = torch.FloatTensor([0]).cuda(0)
-                reg_loss = torch.FloatTensor([0]).cuda(0)
+                be_loss = torch.FloatTensor([0]).cuda(0)
+                aux_loss = torch.FloatTensor([0]).cuda(0)
                 img_loss = torch.stack(img_loss_list,dim=0).mean(dim=0)
             loss = model_codec.loss(reg_loss,img_loss,be_loss,aux_loss)
             ba_loss = torch.stack(bpp_act_list,dim=0).mean(dim=0)
