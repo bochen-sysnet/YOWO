7ddd30a6d740cd902edefc61e6bebbeea27e96f5
Bo Chen <bochen1993cs@gmail.com>
2021-09-22 10:17:42 -0500

    Update main_codec.py

--- main_codec.py
+++ main_codec.py
@@ -90,6 +90,8 @@ if cfg.TRAIN.RESUME_PATH:
         print("No need to load for ", cfg.TRAIN.CODEC_NAME)
     elif cfg.TRAIN.RESUME_CODEC_PATH and os.path.isfile(cfg.TRAIN.RESUME_CODEC_PATH):
         checkpoint = torch.load(cfg.TRAIN.RESUME_CODEC_PATH)
+        print(checkpoint['state_dict']['mv_codec.entropy_bottleneck._quantized_cdf'][0,:],checkpoint['state_dict']['mv_codec.entropy_bottleneck._cdf_length'])
+        print(checkpoint['state_dict']['res_codec.entropy_bottleneck._quantized_cdf'][0,:],checkpoint['state_dict']['res_codec.entropy_bottleneck._cdf_length'])
         cfg.TRAIN.BEGIN_EPOCH = checkpoint['epoch'] + 1
         best_codec_score = checkpoint['score']
         model_codec.load_state_dict(checkpoint['state_dict'])
