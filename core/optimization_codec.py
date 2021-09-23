7240c69938a568dd51ccdf19caf22395e23a05f4
Bo Chen <bochen1993cs@gmail.com>
2021-09-23 11:01:11 -0500

    Update optimization_codec.py

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -96,6 +96,7 @@ def train_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, train_dataset, los
                 img_loss = torch.stack(img_loss_list,dim=0).mean(dim=0)
                 flow_loss = torch.stack(flow_loss_list,dim=0).mean(dim=0)
             loss = model_codec.loss(reg_loss,img_loss,be_loss,aux_loss,flow_loss)
+            print(ba_loss)
             ba_loss = torch.stack(bpp_act_list,dim=0).mean(dim=0)
             metrics = torch.stack(metrics_list,dim=0).mean(dim=0)
             aux_loss_module.update(aux_loss.cpu().data.item(), cfg.TRAIN.BATCH_SIZE)
