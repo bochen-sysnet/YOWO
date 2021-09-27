ac2f990980cc4987144f28d4807edc4af8dfc163
Bo Chen <bochen1993cs@gmail.com>
2021-09-27 09:46:34 -0500

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -185,10 +185,10 @@ class StandardVideoCodecs(nn.Module):
             output_filename = 'tmp/videostreams/output.mp4'
             if self.name == 'x265':
                 libname = 'libx265'
-                cmd = f'/usr/bin/ffmpeg -pix_fmt yuv420p -s {width}x{height} -r {fps} -i pipe: -c:v libx265 -preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" {output_filename}'
+                cmd = f'/usr/bin/ffmpeg -pix_fmt yuv420p -s {width}x{height} -f rawvideo -r {fps} -i pipe: -c:v libx265 -preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" {output_filename}'
             elif self.name == 'x264':
                 libname = 'libx264'
-                cmd = f'/usr/bin/ffmpeg -y -pix_fmt yuv420p -s {width}x{height} -r {fps} -i pipe: -c:v libx264 -preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b strategy 0 -sc threshold 0 -loglevel debug {output_filename}'
+                cmd = f'/usr/bin/ffmpeg -y -pix_fmt yuv420p -s {width}x{height} -f rawvideo -r {fps} -i pipe: -c:v libx264 -preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b strategy 0 -sc threshold 0 -loglevel debug {output_filename}'
             else:
                 print('Codec not supported')
                 exit(1)
