4b429434180fee14bc29a2cca8df72904c9a6bf7
Bo Chen <bochen1993cs@gmail.com>
2021-09-27 09:55:33 -0500

    ev

--- codec/models.py
+++ codec/models.py
@@ -184,12 +184,9 @@ class StandardVideoCodecs(nn.Module):
             GOP = 10
             output_filename = 'tmp/videostreams/output.mp4'
             if self.name == 'x265':
-                libname = 'libx265'
-                cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec {libname} -pix_fmt yuv420p -preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" {output_filename}'
+                cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx265 -pix_fmt yuv420p -preset veryfast -tune zerolatency -x265-params "crf={Q}:keyint={GOP}:verbose=1" {output_filename}'
             elif self.name == 'x264':
-                libname = 'libx264'
-                cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec {libname} -pix_fmt yuv420p -preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b_strategy 0 -sc_threshold 0 -loglevel debug {output_filename}'
-                #cmd = f'/usr/bin/ffmpeg -y -pix_fmt yuv420p -s {width}x{height} -f rawvideo -r {fps} -i pipe: -c:v libx264 -preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b strategy 0 -sc_threshold 0 -loglevel debug {output_filename}'
+                cmd = f'/usr/bin/ffmpeg -y -s {width}x{height} -pixel_format bgr24 -f rawvideo -r {fps} -i pipe: -vcodec libx264 -pix_fmt yuv420p -preset veryfast -tune zerolatency -crf {Q} -g {GOP} -bf 2 -b_strategy 0 -sc_threshold 0 -loglevel debug {output_filename}'
             else:
                 print('Codec not supported')
                 exit(1)
