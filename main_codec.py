5a5c8e8d401b88e2e1d48671f754ea23ca2e9814
Bo Chen <bochen1993cs@gmail.com>
2021-10-14 08:02:51 -0500

    Update main_codec.py

--- main_codec.py
+++ main_codec.py
@@ -52,7 +52,8 @@ pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_g
 logging('Total number of trainable parameters: {}'.format(pytorch_total_params))
 
 # codec model .
-assert cfg.TRAIN.CODEC_NAME in ['MRLVC-RPM-BPG' 'MRLVC-RHP-BPG','RLVC','DVC','RAW','x264','x265'],cfg.TRAIN.CODEC_NAME
+print(cfg.TRAIN.CODEC_NAME)
+assert cfg.TRAIN.CODEC_NAME in ['MRLVC-RPM-BPG' 'MRLVC-RHP-BPG','RLVC','DVC','RAW','x264','x265']
 if cfg.TRAIN.CODEC_NAME in ['MRLVC-RPM-BPG' 'MRLVC-RHP-BPG','RLVC','DVC','RAW']:
     model_codec = LearnedVideoCodecs(cfg.TRAIN.CODEC_NAME)
 elif cfg.TRAIN.CODEC_NAME in ['x264','x265']:
