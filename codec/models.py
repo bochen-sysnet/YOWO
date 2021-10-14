9fe277a2f04605d51abbb697cd757f80df709037
Bo Chen <bochen1993cs@gmail.com>
2021-10-14 13:08:43 -0500

    w

--- codec/models.py
+++ codec/models.py
@@ -524,7 +524,6 @@ class ComprNet(nn.Module):
         
         # calculate bpp (estimated)
         bits_est = self.entropy_bottleneck.get_estimate_bits(likelihoods)
-        print(bits_est,latent.size())
         
         # calculate bpp (actual)
         if self.use_RPM:
