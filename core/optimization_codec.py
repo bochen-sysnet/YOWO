540b5f104d2efa9391796851a2d3ed4f75d92725
Bo Chen <bochen1993cs@gmail.com>
2021-10-16 12:17:27 -0500

    Update optimization_codec.py

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -166,7 +166,7 @@ def train_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, train_dataset, los
         scaler.scale(loss).backward()
         steps = cfg.TRAIN.TOTAL_BATCH_SIZE // cfg.TRAIN.BATCH_SIZE
         if batch_idx % steps == 0:
-            #torch.nn.utils.clip_grad_norm_(model_codec.parameters(),1)
+            torch.nn.utils.clip_grad_norm_(model_codec.parameters(),1)
             scaler.step(optimizer)
             scaler.update()
             optimizer.zero_grad()
