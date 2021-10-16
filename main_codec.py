18ad27f34e5905a07b7e74cf2f074fa11e2a1949
Bo Chen <bochen1993cs@gmail.com>
2021-10-16 07:12:18 -0500

    Update main_codec.py

--- main_codec.py
+++ main_codec.py
@@ -89,7 +89,7 @@ if cfg.TRAIN.RESUME_PATH:
     del checkpoint
     # try to load pretrained model
     if True:
-        pretrained_model_path = "/home/monet/research/YOWO/backup/ucf24/yowo_ucf24_16f_MRLVC-RPM-BPG_best.pth"
+        pretrained_model_path = "/home/monet/research/YOWO/backup/ucf24/ucf_MRLVC_pretrained.pth"
         pre_checkpoint = torch.load(pretrained_model_path)
         model_codec.load_whatever(pre_checkpoint['state_dict'])
     # try to load codec model 
