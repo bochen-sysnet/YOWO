d2ae831c502cc9aa2ad019555d685a6fc072a51c
Bo Chen <bochen1993cs@gmail.com>
2021-10-17 06:48:55 -0500

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -87,7 +87,7 @@ class LearnedVideoCodecs(nn.Module):
         if epoch <= flowEstEpoch:
             self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,1,1,1,0
         elif epoch <= mseEpoch:
-            self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 0,1,0,0,0
+            self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 0,1,0,0.01,0
         elif epoch <= adEpoch:
             self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,1,0,0,1
         else:
