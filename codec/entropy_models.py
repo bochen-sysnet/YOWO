a21b97d5e72e45a74faa6093b00bf3ed1cf25e57
Bo Chen <bochen1993cs@gmail.com>
2021-10-22 10:36:36 -0500

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 from torch import Tensor
 
 from compressai.entropy_models import EntropyModel,GaussianConditional
+from compressai.models import CompressionModel
 import sys, os
 sys.path.append('..')
 import codec.arithmeticcoding as arithmeticcoding
@@ -263,60 +264,15 @@ class RecProbModel(EntropyModel):
             bits_act = torch.FloatTensor([len(b''.join(string))*8]).squeeze(0)
         return bits_act
         
-class RecProbModel_v2(EntropyModel):
-    r"""Entropy bottleneck layer, introduced by J. Ballé, D. Minnen, S. Singh,
-    S. J. Hwang, N. Johnston, in `"Variational image compression with a scale
-    hyperprior" <https://arxiv.org/abs/1802.01436>`_.
-    This is a re-implementation of the entropy bottleneck layer in
-    *tensorflow/compression*. See the original paper and the `tensorflow
-    documentation
-    <https://tensorflow.github.io/compression/docs/entropy_bottleneck.html>`__
-    for an introduction.
-    """
-
-    _offset: Tensor
+class RecProbModel_v2(CompressionModel):
 
     def __init__(
         self,
         channels,
-        name,
-        tail_mass = 1e-9,
-        init_scale = 10,
-        filters = (3, 3, 3, 3),
     ):
-        super().__init__()
+        super().__init__(channels)
 
         self.channels = int(channels)
-        self.filters = tuple(int(f) for f in filters)
-        self.init_scale = float(init_scale)
-        self.tail_mass = float(tail_mass)
-
-        # Create parameters
-        filters = (1,) + self.filters + (1,)
-        scale = self.init_scale ** (1 / (len(self.filters) + 1))
-        channels = self.channels
-
-        for i in range(len(self.filters) + 1):
-            init = np.log(np.expm1(1 / scale / filters[i + 1]))
-            matrix = torch.Tensor(channels, filters[i + 1], filters[i])
-            matrix.data.fill_(init)
-            self.register_parameter(f"_matrix{i:d}", nn.Parameter(matrix))
-
-            bias = torch.Tensor(channels, filters[i + 1], 1)
-            nn.init.uniform_(bias, -0.5, 0.5)
-            self.register_parameter(f"_bias{i:d}", nn.Parameter(bias))
-
-            if i < len(self.filters):
-                factor = torch.Tensor(channels, filters[i + 1], 1)
-                nn.init.zeros_(factor)
-                self.register_parameter(f"_factor{i:d}", nn.Parameter(factor))
-
-        self.quantiles = nn.Parameter(torch.Tensor(channels, 1, 3))
-        init = torch.Tensor([-self.init_scale, 0, self.init_scale])
-        self.quantiles.data = init.repeat(self.quantiles.size(0), 1, 1)
-
-        target = np.log(2 / self.tail_mass - 1)
-        self.register_buffer("target", torch.Tensor([-target, 0, target]))
         
         self.sigma = self.mu = self.prior_latent = None
         self.RPM = RPM(channels)
@@ -326,96 +282,18 @@ class RecProbModel_v2(EntropyModel):
              
     def init_state(self):
         return self.model_states
-
-    def _get_medians(self):
-        medians = self.quantiles[:, :, 1:2]
-        return medians
-
-    def update(self, force = False):
-        # Check if we need to update the bottleneck parameters, the offsets are
-        # only computed and stored when the conditonal model is update()'d.
-        if self._offset.numel() > 0 and not force:
-            return False
-
-        medians = self.quantiles[:, 0, 1]
-
-        minima = medians - self.quantiles[:, 0, 0]
-        minima = torch.ceil(minima).int()
-        minima = torch.clamp(minima, min=0)
-
-        maxima = self.quantiles[:, 0, 2] - medians
-        maxima = torch.ceil(maxima).int()
-        maxima = torch.clamp(maxima, min=0)
-
-        self._offset = -minima
-
-        pmf_start = medians - minima
-        pmf_length = maxima + minima + 1
-
-        max_length = pmf_length.max().item()
-        device = pmf_start.device
-        samples = torch.arange(max_length, device=device)
-
-        samples = samples[None, :] + pmf_start[:, None, None]
-
-        half = float(0.5)
-
-        lower = self._logits_cumulative(samples - half, stop_gradient=True)
-        upper = self._logits_cumulative(samples + half, stop_gradient=True)
-        sign = -torch.sign(lower + upper)
-        pmf = torch.abs(torch.sigmoid(sign * upper) - torch.sigmoid(sign * lower))
-
-        pmf = pmf[:, 0, :]
-        tail_mass = torch.sigmoid(lower[:, 0, :1]) + torch.sigmoid(-upper[:, 0, -1:])
-
-        quantized_cdf = self._pmf_to_cdf(pmf, tail_mass, pmf_length, max_length)
-        self._quantized_cdf = quantized_cdf
-        self._cdf_length = pmf_length + 2
-        return True
+        
+    def update(self, scale_table=None, force=False):
+        if scale_table is None:
+            scale_table = get_scale_table()
+        updated = self.gaussian_conditional.update_scale_table(scale_table, force=force)
+        updated |= super().update(force=force)
+        return updated
 
     def loss(self, RPM_flag):
         if RPM_flag:
             return torch.FloatTensor([0]).squeeze(0).cuda(0)
-        logits = self._logits_cumulative(self.quantiles, stop_gradient=True)
-        loss = torch.abs(logits - self.target).sum()
-        return loss
-
-    def _logits_cumulative(self, inputs, stop_gradient):
-        # TorchScript not yet working (nn.Mmodule indexing not supported)
-        logits = inputs
-        for i in range(len(self.filters) + 1):
-            matrix = getattr(self, f"_matrix{i:d}")
-            if stop_gradient:
-                matrix = matrix.detach()
-            logits = torch.matmul(F.softplus(matrix), logits)
-
-            bias = getattr(self, f"_bias{i:d}")
-            if stop_gradient:
-                bias = bias.detach()
-            logits += bias
-
-            if i < len(self.filters):
-                factor = getattr(self, f"_factor{i:d}")
-                if stop_gradient:
-                    factor = factor.detach()
-                logits += torch.tanh(factor) * torch.tanh(logits)
-                
-        return logits
-
-    @torch.jit.unused
-    def _likelihood(self, inputs):
-        half = float(0.5)
-        v0 = inputs - half
-        v1 = inputs + half
-        lower = self._logits_cumulative(v0, stop_gradient=False)
-        upper = self._logits_cumulative(v1, stop_gradient=False)
-        sign = -torch.sign(lower + upper)
-        sign = sign.detach()
-        likelihood = torch.abs(
-            torch.sigmoid(sign * upper) - torch.sigmoid(sign * lower)
-        )
-        likelihood = torch.clip(likelihood, min=self.tail_mass, max=1 - self.tail_mass)
-        return likelihood
+        return self.aux_loss()
 
     def forward(
         self, x, rpm_hidden, RPM_flag, training = None, stopGradient = True
@@ -428,90 +306,17 @@ class RecProbModel_v2(EntropyModel):
                 self.prior_latent = self.prior_latent.detach()
                 rpm_hidden = rpm_hidden.detach()
             return self.prior_latent, likelihood, rpm_hidden
-        self.prior_latent = torch.round(x)
-        if stopGradient:
-            self.prior_latent = self.prior_latent.detach()
             
-        if training is None:
-            training = self.training
-
-        if not torch.jit.is_scripting():
-            # x from B x C x ... to C x B x ...
-            perm = np.arange(len(x.shape))
-            perm[0], perm[1] = perm[1], perm[0]
-            # Compute inverse permutation
-            inv_perm = np.arange(len(x.shape))[np.argsort(perm)]
-        else:
-            # TorchScript in 2D for static inference
-            # Convert to (channels, ... , batch) format
-            perm = (1, 2, 3, 0)
-            inv_perm = (3, 0, 1, 2)
-
-        x = x.permute(*perm).contiguous()
-        shape = x.size()
-        values = x.reshape(x.size(0), 1, -1)
-
-        # Add noise or quantize
-
-        outputs = self.quantize(
-            values, "noise" if training else "dequantize", self._get_medians()
-        )
-
-        if not torch.jit.is_scripting():
-            likelihood = self._likelihood(outputs)
-            if self.use_likelihood_bound:
-                likelihood = self.likelihood_lower_bound(likelihood)
-        else:
-            # TorchScript not yet supported
-            likelihood = torch.zeros_like(outputs)
-
-        # Convert back to input tensor shape
-        outputs = outputs.reshape(shape)
-        outputs = outputs.permute(*inv_perm).contiguous()
-
-        likelihood = likelihood.reshape(shape)
-        likelihood = likelihood.permute(*inv_perm).contiguous()
-
+        outputs,likelihood = self.entropy_bottleneck(x,training=training)
+        self.prior_latent = outputs
         return outputs, likelihood, rpm_hidden
-
-    @staticmethod
-    def _build_indexes(size):
-        dims = len(size)
-        N = size[0]
-        C = size[1]
-
-        view_dims = np.ones((dims,), dtype=np.int64)
-        view_dims[1] = -1
-        indexes = torch.arange(C).view(*view_dims)
-        indexes = indexes.int()
-
-        return indexes.repeat(N, 1, *size[2:])
-
-    @staticmethod
-    def _extend_ndims(tensor, n):
-        return tensor.reshape(-1, *([1] * n)) if n > 0 else tensor.reshape(-1)
-
-    def compress(self, x):
-        indexes = self._build_indexes(x.size())
-        medians = self._get_medians().detach()
-        spatial_dims = len(x.size()) - 2
-        medians = self._extend_ndims(medians, spatial_dims)
-        medians = medians.expand(x.size(0), *([-1] * (spatial_dims + 1)))
-        return super().compress(x, indexes, medians)
-
-    def decompress(self, strings, size):
-        output_size = (len(strings), self._quantized_cdf.size(0), *size)
-        indexes = self._build_indexes(output_size).to(self._quantized_cdf.device)
-        medians = self._extend_ndims(self._get_medians().detach(), len(size))
-        medians = medians.expand(len(strings), *([-1] * (len(size) + 1)))
-        return super().decompress(strings, indexes, medians.dtype, medians)
         
     def get_actual_bits(self, x, RPM_flag):
         if RPM_flag:
             indexes = self.gaussian_conditional.build_indexes(self.sigma)
             string = self.gaussian_conditional.compress(x, indexes, means=self.mu)
         else:
-            string = self.compress(x)
+            string = self.entropy_bottleneck.compress(x)
         bits_act = torch.FloatTensor([len(b''.join(string))*8]).squeeze(0)
         return bits_act
         
