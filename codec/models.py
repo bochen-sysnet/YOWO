624ec468dc3cacff7b5562d343906e0c38b04113
Bo Chen <bochen1993cs@gmail.com>
2021-10-25 10:09:31 -0500

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -588,11 +588,12 @@ class PositionalEncoding(nn.Module):
 
     def __init__(self, d_model, max_len=100):
         super(PositionalEncoding, self).__init__()       
-        pe = torch.zeros(max_len, d_model, 1, 1)
+        pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(2).unsqueeze(3)
         pe.requires_grad = False
         self.register_buffer('pe', pe)
 
