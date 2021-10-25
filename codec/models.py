743c0bfe9108e5a8fa5344bf8884fc684cc82cf5
Bo Chen <bochen1993cs@gmail.com>
2021-10-25 16:17:02 -0500

    train 5

--- codec/models.py
+++ codec/models.py
@@ -82,7 +82,7 @@ class LearnedVideoCodecs(nn.Module):
         # setup training weights
         if epoch <= -1:
             self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,1,1,1,1
-        elif epoch <= 5:
+        elif epoch <= 4:
             self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,0,1,0,0
         else:
             self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,10,.01,.01,0
