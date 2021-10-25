6fdb9b434d1e974c4986bd44e1c967a4eab00af0
Bo Chen <bochen1993cs@gmail.com>
2021-10-25 17:09:31 -0500

    test

--- codec/models.py
+++ codec/models.py
@@ -85,7 +85,7 @@ class LearnedVideoCodecs(nn.Module):
         elif epoch <= 4:
             self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,0,1,0,0
         else:
-            self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,10,.01,.01,0
+            self.gamma_0, self.gamma_1, self.gamma_2, self.gamma_3, self.gamma_4 = 1,10,1,1,0
             
         # set up GOP
         # epoch >=1 means pretraining on I-frame compression
