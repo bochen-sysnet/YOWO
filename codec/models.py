81d46d68ffdac31888d090f1e1b95b3859f386aa
Bo Chen <bochen1993cs@gmail.com>
2021-11-03 01:12:25 -0500

    c

--- codec/models.py
+++ codec/models.py
@@ -319,7 +319,7 @@ class DCVC(nn.Module):
         y = self.ctx_encoder(torch.cat((x, context), axis=1))
         
         # entropy model
-        y_hat, likelihoods = self.entropy_bottleneck(y, context, training=self.training)
+        y_hat, likelihoods = self.entropy_bottleneck(y, prior, training=self.training)
         y_est = self.entropy_bottleneck.get_estimate_bits(likelihoods)
         y_act = self.entropy_bottleneck.get_actual_bits(y)
         y_aux = self.entropy_bottleneck.loss()
