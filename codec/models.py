fd3253dff3e29c6372ab7cc8befee418d1d2622e
Bo Chen <bochen1993cs@gmail.com>
2021-11-03 21:46:01 -0500

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -1230,8 +1230,7 @@ class SLVC2(nn.Module):
         aux_loss = (ref_aux + y_aux)/2
         # calculate metrics/loss
         metrics = calc_metrics(x, x_hat, use_psnr)
-        rec_loss = calc_loss(x, x_hat, use_psnr)
-        img_loss = (self.gamma_rec*rec_loss + self.gamma_warp*warp_loss)/(self.gamma_rec+self.gamma_warp) 
+        img_loss = calc_loss(x, x_hat, self.r, use_psnr)
         # flow loss
         flow_loss = (l0+l1+l2+l3+l4)/5*1024
         # hidden
