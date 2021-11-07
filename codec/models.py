caf990a42aa3e38ead0d03c9a8970fe3a6b391bf
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 11:54:59 -0600

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -331,6 +331,7 @@ class DCVC(nn.Module):
         bpp_est = (mv_est + y_est.cuda(0))/(h * w * bs)
         # actual bits
         bpp_act = (mv_act + y_act.cuda(0))/(h * w * bs)
+        print(float(mv_est),float(y_est),float(mv_act),float(y_act))
         # auxilary loss
         aux_loss = (mv_aux + y_aux.cuda(0))/2
         # calculate metrics/loss
