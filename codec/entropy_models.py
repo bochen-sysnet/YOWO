0fa996e17385714150aa5d78b8251bdb322f292e
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 15:46:28 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -260,7 +260,7 @@ def test(name = 'RPM'):
         else:
             x_hat, likelihoods = net(x,x,training=True)
             
-        string = net.compress(torch.zeros(1,channels,14,14))
+        string = net.compress(x)
         bits_act = net.get_actual_bits(string)
         x_hat2 = net.decompress(string, x.size()[-2:])
         mse2 = torch.mean(torch.pow(x_hat-x_hat2,2))
