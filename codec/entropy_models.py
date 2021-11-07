f20a1e435c691fb7043c84d91a1ee5553dea7b08
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 11:31:26 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -265,7 +265,7 @@ def test(name = 'RPM'):
         net.update(force=True)
         if name == 'RPM':
             net.set_RPM(rpm_flag)
-            x_hat, likelihoods, rpm_hidden = net(x,rpm_hidden,training=False)
+            x_hat, likelihoods, rpm_hidden = net(x,False,training=False)
         else:
             x_hat, likelihoods = net(x,x,training=True)
             
