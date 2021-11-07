064a9daed401de3c665c12e9b2037904f0d94e80
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 17:36:43 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -157,7 +157,7 @@ class JointAutoregressiveHierarchicalPriors(CompressionModel):
         )
         self.sigma, self.mu = torch.split(gaussian_params, self.channels, dim=1) # for fast compression
         # post-process sigma to stablize training
-        self.sigma = torch.exp(self.sigma)
+        self.sigma = torch.exp(self.sigma)/10
         x_hat,x_likelihood = self.gaussian_conditional(x, self.sigma, means=self.mu, training=training)
         return x_hat, (x_likelihood,z_likelihood)
         
