a63da08502fd8bf20ca8e4b3dfe5d7e3cb659402
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 10:37:08 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -281,4 +281,5 @@ if __name__ == '__main__':
     string = net.entropy_bottleneck.compress(x)
     x2 = net.entropy_bottleneck.decompress(string, x.size())
     mse2 = torch.mean(torch.pow(x2-x3,2))
+    print(x2.size(),x3.size())
     print(float(mse2),x3,x2)
