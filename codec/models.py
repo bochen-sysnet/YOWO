94b2187ab818c92dddf15af97f02f08ae24944d8
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 13:37:27 -0600

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -771,7 +771,7 @@ class ComprNet(nn.Module):
         bits_est = self.entropy_bottleneck.get_estimate_bits(likelihoods)
         
         # calculate bpp (actual)
-        img = torch.rand(1,self.channels,14,14)
+        img = torch.rand(1,self.channels,14,14).cuda()
         latent_string = self.entropy_bottleneck.compress(img)
         bits_act = self.entropy_bottleneck.get_actual_bits(latent_string)
         print(bits_est,bits_act)
