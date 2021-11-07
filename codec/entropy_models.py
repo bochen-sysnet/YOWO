f32d461dfda1854e4d4f9cf41a40d7fdeffd06ad
Bo Chen <bochen1993cs@gmail.com>
2021-11-07 13:48:37 -0600

    d

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -75,6 +75,8 @@ class RecProbModel(CompressionModel):
         
     def compress(self, x):
         if self.RPM_flag:
+            print(x)
+            print('sigma',self.sigma)
             indexes = self.gaussian_conditional.build_indexes(self.sigma)
             string = self.gaussian_conditional.compress(x, indexes, means=self.mu)
         else:
--- codec/models.py
+++ codec/models.py
@@ -771,7 +771,7 @@ class ComprNet(nn.Module):
         bits_est = self.entropy_bottleneck.get_estimate_bits(likelihoods)
         
         # calculate bpp (actual)
-        latent_string = self.entropy_bottleneck.compress(latent*1000)
+        latent_string = self.entropy_bottleneck.compress(latent)
         bits_act = self.entropy_bottleneck.get_actual_bits(latent_string)
         print(bits_est,bits_act,latent)
 
