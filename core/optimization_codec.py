8d006f0c574dd9b23e7d53dc2632ba0690e73037
Bo Chen <bochen1993cs@gmail.com>
2021-11-09 14:52:38 -0600

    Update optimization_codec.py

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -405,7 +405,7 @@ def test_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, test_dataset, loss_
         for j in range(batch_size):
             data_idx = batch_idx*batch_size+j
             # compress one batch of the data
-            test_dataset.preprocess(data_idx, model_codec)
+            test_dataset.preprocess(data_idx, model_codec, batch_size-j)
             # read one clip
             f,d,t,be,il,a,fl,ba,p,m = test_dataset[data_idx]
             frame_idx.append(f)
