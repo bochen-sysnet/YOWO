dd00c2dc898f5dd3c01301fd88afd3ba3b213eea
Bo Chen <bochen1993cs@gmail.com>
2021-11-09 15:12:05 -0600

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -199,7 +199,6 @@ def parallel_compression(model, _range, cache):
             x = torch.stack(img_list, dim=0)
             n = len(idx_list)
             x_hat, _, bpp_est, img_loss, aux_loss, flow_loss, bpp_act, psnr, msssim = model(x, None)
-            print(float(bpp_est),float(bpp_act),n)
             for pos,j in enumerate(idx_list):
                 cache['clip'][j] = x_hat[pos].squeeze(0).detach()
                 cache['img_loss'][j] = img_loss/n
