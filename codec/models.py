f06a7124c3e7965ce57f6d5312b3311f34eb09cc
Bo Chen <bochen1993cs@gmail.com>
2021-11-09 15:21:56 -0600

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -1118,7 +1118,7 @@ class SPVC(nn.Module):
         bpp_est = (ref_est + mv_est.cuda(0) + res_est.cuda(0))/(h * w * bs)
         # actual bits
         bpp_act = (ref_act + mv_act.cuda(0) + res_act.cuda(0))/(h * w * bs)
-        #print(float(bpp_est),float(bpp_act))
+        print(float(bpp_est),float(bpp_act))
         # auxilary loss
         aux_loss = (ref_aux + mv_aux.cuda(0) + res_aux.cuda(0))/3
         # calculate metrics/loss
