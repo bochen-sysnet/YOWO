17decfebd83936d6f20337acb2dd58645ca84e7c
Bo Chen <bochen1993cs@gmail.com>
2021-11-11 21:24:48 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -162,7 +162,7 @@ class MeanScaleHyperPriors(CompressionModel):
         )
         
         self.h_s2 = nn.Sequential(
-            nn.ConvTranspose2d(channels, channels, kernel_size=5, stride=2, padding=2),
+            nn.ConvTranspose2d(channels, channels, kernel_size=5, stride=2, padding=2, output_padding=1),
             nn.LeakyReLU(inplace=True),
             nn.Conv2d(channels, channels*2, kernel_size=3, stride=1, padding=1),
         )
