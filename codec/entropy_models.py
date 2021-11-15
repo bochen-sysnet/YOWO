46f3a255f70ccbf2f17eaf5bcdc4a43e9c90d568
Bo Chen <bochen1993cs@gmail.com>
2021-11-15 14:18:22 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -172,7 +172,7 @@ def myupdate(self, force = False):
 from compressai._CXX import pmf_to_quantized_cdf as _pmf_to_quantized_cdf
 
 def pmf_to_quantized_cdf(pmf, precision = 16):
-    cdf = _pmf_to_quantized_cdf(pmf.tolist(), 32)
+    cdf = _pmf_to_quantized_cdf(pmf.tolist(), 16)
     cdf = torch.IntTensor(cdf)
     return cdf
     
