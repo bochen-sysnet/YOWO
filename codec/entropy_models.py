92392d29333761904a94fd77ef35c36ed29e5079
Bo Chen <bochen1993cs@gmail.com>
2021-11-15 13:48:03 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -203,6 +203,7 @@ class MeanScaleHyperPriors(CompressionModel):
     def update(self, scale_table=None, force=False):
         print('start')
         updated = self.gaussian_conditional.update_scale_table(self.scale_table, force=force)
+        print('mid')
         updated |= super().update(force=force)
         print('end')
         return updated
