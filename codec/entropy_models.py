0494e032ba8e9825c9b50fd9ede517e8bf1108a2
Bo Chen <bochen1993cs@gmail.com>
2021-11-20 15:53:02 -0600

    Update entropy_models.py

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -292,7 +292,7 @@ class MeanScaleHyperPriors(CompressionModel):
         self.sigma, self.mu = torch.split(gaussian_params, self.channels, dim=1) # for fast compression
         # post-process sigma to stablize training
         self.sigma = torch.maximum(self.sigma, torch.FloatTensor([-7.0]).to(x.device))
-        self.sigma = torch.exp(self.sigma)*10
+        self.sigma = torch.exp(self.sigma)
         x_hat,x_likelihood = self.gaussian_conditional(x, self.sigma, means=self.mu, training=training)
         return x_hat, (x_likelihood,z_likelihood)
         
