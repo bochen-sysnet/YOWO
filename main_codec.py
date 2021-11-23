b459894d411a6607f76ad36e24d6f338ef507859
Bo Chen <bochen1993cs@gmail.com>
2021-11-23 14:07:10 -0600

    Update main_codec.py

--- main_codec.py
+++ main_codec.py
@@ -82,7 +82,7 @@ if cfg.TRAIN.RESUME_PATH:
     print("===================================================================")
     del checkpoint
     # try to load codec model 
-    if cfg.TRAIN.CODEC_NAME in ['x265', 'x264', 'RAW']:
+    if 'x26' in cfg.TRAIN.CODEC_NAME or 'RAW' == cfg.TRAIN.CODEC_NAME:
         # nothing to load
         print("No need to load for ", cfg.TRAIN.CODEC_NAME)
     elif cfg.TRAIN.CODEC_NAME in ['SCVC','SPVC_v2']:
