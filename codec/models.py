bdfa3ca5a285dee16e6f7c6d27db681a14e88590
Bo Chen <bochen1993cs@gmail.com>
2021-11-23 11:47:59 -0600

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -1726,7 +1726,7 @@ class ResBlockB(nn.Module):
         
 def test_batch_proc(name = 'SPVC'):
     print('test',name)
-    batch_size = 6
+    batch_size = 7
     h = w = 224
     channels = 64
     x = torch.randn(batch_size,3,h,w).cuda()
