00688b82c8be792ea68cdcc750b12b170f31c607
Bo Chen <bochen1993cs@gmail.com>
2021-11-27 07:50:44 -0600

    test

--- core/optimization_codec.py
+++ core/optimization_codec.py
@@ -135,6 +135,7 @@ def train_ucf24_jhmdb21_codec(cfg, epoch, model, model_codec, train_dataset, los
     frame_idx = []; data = []; target = []; img_loss_list = []; aux_loss_list = []
     bpp_est_list = []; bpp_act_list = []; psnr_list = []; msssim_list = []
     for batch_idx,_ in enumerate(train_iter):
+        if batch_idx<=21000:continue
         # align batches
         for j in range(batch_size):
             data_idx = batch_idx*batch_size+j
