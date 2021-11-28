206ec894b9dba38909d6d8b7997d280245ca3da6
Bo Chen <bochen1993cs@gmail.com>
2021-11-28 11:27:45 -0600

    Update models.py

--- codec/models.py
+++ codec/models.py
@@ -265,7 +265,7 @@ def parallel_compression(model, ranges, cache):
         for pos,j in enumerate(idx_list):
             cache['clip'][j] = x_hat[pos].squeeze(0).detach()
             cache['img_loss'][j] = img_loss[pos]
-            cache['aux'][j] = aux_loss[pos
+            cache['aux'][j] = aux_loss[pos]
             cache['bpp_est'][j] = bpp_est[pos]
             cache['psnr'][j] = psnr[pos]
             cache['msssim'][j] = msssim[pos]
