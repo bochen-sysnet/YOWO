d288e5d50185bdba915ee1a03a2f1ce7cad43297
Bo Chen <bochen1993cs@gmail.com>
2021-11-30 23:37:47 -0600

    d

--- codec/entropy_models.py
+++ codec/entropy_models.py
@@ -219,8 +219,6 @@ class MeanScaleHyperPriors(CompressionModel):
             z = st_attention(z,self.s_attn_a,self.t_attn_a)
         z = self.h_a2(z)
         z_hat, z_likelihood = self.entropy_bottleneck(z)
-        print('z',z[0])
-        print('zhat',z_hat[0])
         self.z = z # for fast compression
             
         g = self.h_s1(z_hat)
@@ -276,6 +274,9 @@ class MeanScaleHyperPriors(CompressionModel):
         z = self.h_a2(z)
         z_string = self.entropy_bottleneck.compress(z)
         z_hat = self.entropy_bottleneck.decompress(z_string, z.size()[-2:])
+        
+        print('z',z[0])
+        print('zhat',z_hat[0])
         
         g = self.h_s1(z_hat)
         if self.useAttention:
