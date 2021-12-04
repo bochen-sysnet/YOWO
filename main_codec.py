243acd949a05bc079b212cd6a58252a1b14b060c
Bo Chen <bochen1993cs@gmail.com>
2021-12-04 14:09:58 -0600

    Update main_codec.py

--- main_codec.py
+++ main_codec.py
@@ -86,7 +86,7 @@ if cfg.TRAIN.RESUME_PATH:
     elif cfg.TRAIN.CODEC_NAME in ['SCVC','DCVC']:
         # load what exists
         print("Load whatever exists for",cfg.TRAIN.CODEC_NAME)
-        pretrained_model_path = "/home/monet/research/YOWO/backup/ucf24/yowo_ucf24_16f_SPVC_best.pth"
+        pretrained_model_path = "/home/monet/research/YOWO/backup/ucf24/yowo_ucf24_16f_DCVC_best.pth"
         checkpoint = torch.load(pretrained_model_path)
         load_state_dict_whatever(model_codec, checkpoint['state_dict'])
         del checkpoint
