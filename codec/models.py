fe381cd7098b5d095081518d58ce2ab0433b81c3
Bo Chen <bochen1993cs@gmail.com>
2021-12-03 21:28:20 -0600

    spvc-i

--- codec/models.py
+++ codec/models.py
@@ -89,7 +89,7 @@ def update_training(model, epoch):
     else:
         model.r_img, model.r_bpp, model.r_aux = 0,1,1
         model.r_rec, model.r_flow, model.r_warp, model.r_mc = 1,0,0,0
-        model.r_app = 64 # [1,2,4,8]
+        model.r_app = 0 # [1,2,4,8]
     
     # whether to compute action detection
     doAD = True if model.r_app > 0 else False
